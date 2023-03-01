45a0b43791fce25b41872a2033dda27aecea5631
kubealex <al.rossi87@gmail.com>
2023-03-01 10:02:33 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
-import {Button, PanelMain, PanelMainBody, Panel, TextInput} from '@patternfly/react-core';
+import React from 'react';
+import IndexPage from './components/IndexPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
-
-  const inputRef = useRef(null)
-  const [labelText, setLabelText] = useState('')
-
-  function renderPanel() {
-    //alert('Button was pressed?');
-    setLabelText(inputRef.current.value)
-  }
-
-  const BasicPanel = (props) => {
-    return(
-    <Panel>
-      <PanelMain>
-        <PanelMainBody>{props.value}</PanelMainBody>
-      </PanelMain>
-    </Panel>)
-  };
-
-  const TextInputBasic = () => {
-    const [textValue, setValue] = React.useState('');
-    return(
-    <div style={{width:300+'px'}} >
-    <TextInput ref={inputRef} value={textValue} type="text" onChange={textValue => setValue(textValue)} aria-label="Text sample" />
-    </div>
-    )
-  };
-
+export default function App() {
   return (
-    <div className="App center">
-      <header className="App-header">
-        <TextInputBasic />
-        <Button variant="primary" onClick={renderPanel}> Render panel </Button>
-        <BasicPanel value={labelText}/>
-      </header>
-    </div>
-  );
+    <Router>
+    <Routes>
+      <Route path='/' element={<IndexPage/>} />
+    </Routes>
+    </Router>
+)
 }
-
-export default App;
---
+++ src/components/IndexPage.js
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react';
+import './IndexPage.css';
+import {Button, PanelMain, PanelMainBody, Panel, TextInput} from '@patternfly/react-core';
+
+export default function IndexPage() {
+
+  const inputRef = useRef(null)
+  const [labelText, setLabelText] = useState('')
+
+  function renderPanel() {
+    //alert('Button was pressed?');
+    setLabelText(inputRef.current.value)
+  }
+
+  const BasicPanel = (props) => {
+    return(
+    <Panel variant='raised'>
+      <PanelMain>
+        <PanelMainBody>{props.value}</PanelMainBody>
+      </PanelMain>
+    </Panel>)
+  };
+
+  const TextInputBasic = () => {
+    const [textValue, setValue] = React.useState('');
+    return(
+    <div style={{width:300+'px'}} >
+    <TextInput ref={inputRef} value={textValue} type="text" onChange={textValue => setValue(textValue)} aria-label="Text sample" />
+    </div>
+    )
+  };
+
+  return (
+    <div className="App center">
+      <header className="App-header">
+        <TextInputBasic />
+        <Button variant="primary" onClick={renderPanel}> Render panel </Button>
+        <BasicPanel value={labelText}/>
+      </header>
+    </div>
+  );
+}
+
