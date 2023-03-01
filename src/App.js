47693f6d5769aaf0d3e7405a6cc92a457815270a
kubealex <al.rossi87@gmail.com>
2023-03-01 15:29:51 +0100

    prettified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import IndexPage from './components/pages/IndexPage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from "react";
+import IndexPage from "./components/pages/IndexPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-    <Routes>
-      <Route path='/' element={<IndexPage/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
     </Router>
-)
+  );
 }
--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -1,43 +1,62 @@
-import React, { useRef, useState } from 'react';
-import './IndexPage.css';
-import {Button, PanelMain, PanelMainBody, Panel, TextInput} from '@patternfly/react-core';
+import React, { useRef, useState } from "react";
+import "./IndexPage.css";
+import {
+  Button,
+  PanelMain,
+  PanelMainBody,
+  Panel,
+  TextInput,
+  Divider,
+} from "@patternfly/react-core";
 
 export default function IndexPage() {
-
-  const inputRef = useRef(null)
-  const [labelText, setLabelText] = useState('')
+  const inputRef = useRef(null);
+  const [labelText, setLabelText] = useState("");
 
   function renderPanel() {
     //alert('Button was pressed?');
-    setLabelText(inputRef.current.value)
+    setLabelText(inputRef.current.value);
   }
 
+  const DividerUsingDiv = () => <Divider component="div" />;
+
   const BasicPanel = (props) => {
-    return(
-    <Panel variant='raised'>
-      <PanelMain>
-        <PanelMainBody>{props.value}</PanelMainBody>
-      </PanelMain>
-    </Panel>)
+    return (
+      <Panel variant="raised">
+        <PanelMain>
+          <PanelMainBody>{props.value}</PanelMainBody>
+        </PanelMain>
+      </Panel>
+    );
   };
 
   const TextInputBasic = () => {
-    const [textValue, setValue] = React.useState('');
-    return(
-    <div style={{width:300+'px'}} >
-    <TextInput ref={inputRef} value={textValue} type="text" onChange={textValue => setValue(textValue)} aria-label="Text sample" />
-    </div>
-    )
+    const [textValue, setValue] = React.useState("");
+    return (
+      <div style={{ width: 300 + "px" }}>
+        <TextInput
+          ref={inputRef}
+          value={textValue}
+          type="text"
+          onChange={(textValue) => setValue(textValue)}
+          aria-label="Text sample"
+        />
+      </div>
+    );
   };
 
   return (
     <div className="App center">
       <header className="App-header">
         <TextInputBasic />
-        <Button variant="primary" onClick={renderPanel}> Render panel </Button>
-        <BasicPanel value={labelText}/>
+        <DividerUsingDiv />
+        <Button variant="primary" onClick={renderPanel}>
+          {" "}
+          Render panel{" "}
+        </Button>
+        <DividerUsingDiv />
+        <BasicPanel value={labelText} />
       </header>
     </div>
   );
 }
-
