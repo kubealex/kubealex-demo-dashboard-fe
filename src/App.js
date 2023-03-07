89d88c1ae9244bbb8efc041fdd5324fc27de882b
kubealex <al.rossi87@gmail.com>
2023-03-07 11:37:03 +0100

    Creating rest page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import IndexPage from "./components/pages/IndexPage";
+import RestPage from "./components/pages/RestPage";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -7,6 +9,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route path="/rest" element={<RestPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/pages/RestPage.js
@@ -0,0 +1,57 @@
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import "../../assets/IndexPage.css";
+import {
+  Button,
+  PanelMain,
+  PanelMainBody,
+  Panel,
+  Divider,
+} from "@patternfly/react-core";
+
+export default function IndexPage() {
+  //const [labelText, setLabelText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const fetchData = async () => {
+    try {
+    setIsLoading(true);
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    setPosts(data);
+  } finally {
+  setIsLoading(false);
+}
+  }
+
+  const DividerUsingDiv = () => <Divider component="div" />;
+
+  const BasicPanel = (props) => {
+    return (
+      <Panel variant="raised">
+        <PanelMain>
+            <PanelMainBody>{props.value}</PanelMainBody>
+        </PanelMain>
+      </Panel>
+    );
+  };
+
+  console.log(posts);
+
+  return (
+    <div className="App center">
+      <header className="App-header">
+        <DividerUsingDiv />
+        <Button variant="primary" onClick={fetchData}>
+          Render panel
+        </Button>
+        <DividerUsingDiv />
+        {posts.map((post) => {
+          return <BasicPanel value={post.userId} />
+        }
+        )}
+      </header>
+    </div>
+  );
+}
